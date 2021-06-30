b74b7c94f398530d4dc520ab3c218aba9658ed87
bogdanlotuk <bogdanlotuk@gmail.com>
2021-06-30 14:03:05 +0300

    our-stage-slider

---
+++ app/js/main.js
@@ -0,0 +1,76 @@
+let mySwiper2 = new Swiper (".swiper-container.swiper-our-staging", {
+    loop: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 0
+        }
+    },
+})
+let mySwiper3 = new Swiper (".swiper-container.swiper-partners-first", {
+    loop: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    },
+})
+
+let mySwiper4 = new Swiper (".swiper-container.swiper-partners-second", {
+    loop: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    },
+})

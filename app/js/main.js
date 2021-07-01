84005e85c4c8314499a82356650a320e18c0388a
bogdanlotuk <bogdanlotuk@gmail.com>
2021-07-01 14:36:01 +0300

    media

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,43 @@
+let mySwiper1 = new Swiper (".swiper-container.swiper-header", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    effect: 'cube',
+    navigation: {
+        nextEl: '.swiper-buttons-next',
+        prevEl: '.swiper-buttons-prev',
+    },
+    
+});
 let mySwiper2 = new Swiper (".swiper-container.swiper-our-staging", {
     loop: true,
     breakpoints: {
         320: {
-            slidesPerView: 1,
-            spaceBetween: 20
+            slidesPerView: 1.1,
+            spaceBetween: 0
         },
         576: {
-            slidesPerView: 2,
-            spaceBetween: 20
+            slidesPerView: 1.3,
+            spaceBetween: 0
         },
         768: {
-            slidesPerView: 2,
-            spaceBetween: 20
+            slidesPerView: 1.6,
+            spaceBetween: 0
         },
         992: {
+            slidesPerView: 1.6,
+            spaceBetween: 0
+        },
+        1200: {
             slidesPerView: 3,
-            spaceBetween: 30
+            spaceBetween: 0
         },
         1400: {
             slidesPerView: 4,
             spaceBetween: 0
         }
     },
-})
+});
 let mySwiper3 = new Swiper (".swiper-container.swiper-partners-first", {
     loop: true,
     breakpoints: {
@@ -47,7 +62,7 @@ let mySwiper3 = new Swiper (".swiper-container.swiper-partners-first", {
             spaceBetween: 30
         }
     },
-})
+});
 
 let mySwiper4 = new Swiper (".swiper-container.swiper-partners-second", {
     loop: true,
@@ -73,4 +88,4 @@ let mySwiper4 = new Swiper (".swiper-container.swiper-partners-second", {
             spaceBetween: 30
         }
     },
-})
+});
